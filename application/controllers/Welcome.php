f6410597de6cf76084dd6db6c6d323887a876229
Manuel Gonzales <manuel.gonzales@shaw.ca>
2015-03-27 00:33:37 -0700

    got xml files out of data. create orders Signed-off-by:Manuel Gonzales <manuel.gonzales@shaw.ca>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,7 +21,39 @@ class Welcome extends Application {
     function index()
     {
 	// Build a list of orders
-	
+	$files = firectory_map('./data/');        
+        
+        foreach ($files as $file)
+        {
+            
+           $extension = '.xml';
+           
+            if(substr_compare($file,$extension,strlen($file)-strlen($extension),
+                 strlen($extension)) === 0 && $file !== 'menu.xml')
+             {
+                 // prepare the order file name minus extension
+                 $order_file = $file;
+                 $order_file = substr($order_file,0,strlen($order_file)-4);
+                 // add the order to array of orders
+                 $order_files[] = $order_file;
+             }
+         }
+ 
+         // build a list of orders
+         $orders = array();
+         foreach($order_files as $order_file)
+         {
+             // build the order
+             $order = new stdclass();
+             $order->name = $order_file;
+             $order->path   = 'welcome/order/'.$order_file;
+ 
+             // add the order to array of orders
+             $orders[] = $order;
+         }
+         
+         $this->data['orders'] = $orders;
+
 	// Present the list to choose from
 	$this->data['pagebody'] = 'homepage';
 	$this->render();
