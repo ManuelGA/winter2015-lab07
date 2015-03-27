ea167a5605c751732b2e5ae96ea5c0a1e1534190
Manuel Gonzales <manuel.gonzales@shaw.ca>
2015-03-27 00:33:50 -0700

    Set up autoload and homepage
    Signed-off-by:Manuel Gonzales <manuel.gonzales@shaw.ca>

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,3 +1,11 @@
 <div class="row">
-    Burger Bonanza?
+    <h2>Barker Bob's Burger Bar</h2>
+    <div>
+        <h4>List of Orders</h4>
+        {orders}
+        <div>
+            <a href="{path}">{name}</a>
+        </div>        
+        {/orders}        
+    </div>
 </div>
